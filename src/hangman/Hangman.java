76fecb0ef8669dd18fbf0b306044be378a1d2526
Plamena Georgieva <plmng13@gmail.com>
2015-01-29 21:49:32 +0200

    hangman game

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -9,7 +9,6 @@ public class Hangman implements Runnable {
 	public void run() {
 		new HangmanFrame(new HangmanModel());
     }
-     
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Hangman());
     }
